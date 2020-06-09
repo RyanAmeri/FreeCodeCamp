98a54d50db1659a983bc81447046b6170f7984a8
Ryan Ameri <ryan@internettradie.com.au>
2020-06-10 06:39:28 +1000

    implemented telephoneNumberValidator.js I struggled with the regexp and had to look at the solution. I understand the regexp, but still a long way to go to be able to write something like this

---
+++ algorithm_and_data_structures_projects/telephoneNumberValidator.js
@@ -0,0 +1,5 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/telephone-number-validator
+function telephoneCheck(str) {
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
+  }
